914cb11268054876cf138deb51da493383e50af0
fgilles <fgilles@corp.free.fr>
2022-04-15 23:54:13 +0200

    ajout git config

--- install.sh
+++ install.sh
@@ -31,6 +31,11 @@ ln -sf "$DOTFILES/zsh/aliases" "$XDG_CONFIG_HOME/zsh/aliases"
 rm -rf "$XDG_CONFIG_HOME/zsh/external"
 ln -sf "$DOTFILES/zsh/external" "$XDG_CONFIG_HOME/zsh"
 
+#######
+# Git #
+#######
+ln -sf "$DOTFILES/git/.gitconfig" "$HOME/.gitconfig"
+
 #########
 # Fonts #
 #########
