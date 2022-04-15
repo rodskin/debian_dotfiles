48b9ca14855fc65f88a7e19a4e8ea39b99029d4a
fgilles <fgilles@corp.free.fr>
2022-04-15 23:48:20 +0200

    first commit:

---
+++ install.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+########
+# nvim #
+########
+mkdir -p "$XDG_CONFIG_HOME/nvim"
+mkdir -p "$XDG_CONFIG_HOME/nvim/undo"
+ln -sf "$DOTFILES/nvim/init.vim" "$XDG_CONFIG_HOME/nvim"
+
+#######
+# X11 #
+#######
+rm -rf "$XDG_CONFIG_HOME/X11"
+ln -s "$DOTFILES/X11" "$XDG_CONFIG_HOME"
+
+######
+# i3 #
+######
+rm -rf "$XDG_CONFIG_HOME/i3"
+ln -s "$DOTFILES/i3" "$XDG_CONFIG_HOME"
+rm -rf "$XDG_CONFIG_HOME/i3blocks"
+ln -s "$DOTFILES/i3blocks" "$XDG_CONFIG_HOME"
+
+#######
+# ZSH #
+#######
+mkdir -p "$XDG_CONFIG_HOME/zsh"
+ln -sf "$DOTFILES/zsh/.zshenv" "$HOME"
+ln -sf "$DOTFILES/zsh/.zshrc" "$XDG_CONFIG_HOME/zsh"
+ln -sf "$DOTFILES/zsh/aliases" "$XDG_CONFIG_HOME/zsh/aliases"
+rm -rf "$XDG_CONFIG_HOME/zsh/external"
+ln -sf "$DOTFILES/zsh/external" "$XDG_CONFIG_HOME/zsh"
+
+#########
+# Fonts #
+#########
+
+mkdir -p "$XDG_DATA_HOME"
+cp -rf "$DOTFILES/fonts" "$XDG_DATA_HOME"
+
+#########
+# Dunst #
+#########
+mkdir -p "$XDG_CONFIG_HOME/dunst"
+ln -sf "$DOTFILES/dunst/dunstrc" "$XDG_CONFIG_HOME/dunst/dunstrc"
---
+++ zsh/.zshenv
@@ -0,0 +1,20 @@
+# For dotfiles
+export XDG_CONFIG_HOME="$HOME/.config"
+export DOTFILES="$HOME/dotfiles"
+
+# For specific data
+export XDG_DATA_HOME="$XDG_CONFIG_HOME/local/share"
+
+# For cached files
+export XDG_CACHE_HOME="$XDG_CONFIG_HOME/cache"
+
+# For Favorite editor
+export EDITOR="nvim"
+export VISUAL="nvim"
+
+export ZDOTDIR="$XDG_CONFIG_HOME/zsh"
+# History filepath
+export HISTFILE="$ZDOTDIR/.zhistory"
+# Maximum events for internal history
+export SAVEHIST=10000
+
