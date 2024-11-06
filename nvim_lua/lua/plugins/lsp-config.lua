572461f361522ad7b0db97fa1df8b2bf208fd992
Rodskin <rod.gilles@gmail.com>
2024-11-06 14:42:59 +0100

    update

--- nvim_lua/lua/plugins/lsp-config.lua
+++ nvim_lua/lua/plugins/lsp-config.lua
@@ -66,6 +66,8 @@ return {
       -- shortcuts
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
     end,
   },
--- nvim_lua/lua/plugins/treesitter.lua
+++ nvim_lua/lua/plugins/treesitter.lua
@@ -1,24 +1,25 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local config = require("nvim-treesitter.configs")
-		config.setup({
-			ensure_installed = {
-        "lua",
-        "vimdoc",
-        "json",
-        "http",
-        "query",
-        "javascript",
-        "html",
-        "php",
-        "go",
-        "bash",
-      },
-			sync_install = false,
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+        local config = require("nvim-treesitter.configs")
+        config.setup({
+            ensure_installed = {
+                "lua",
+                "vimdoc",
+                "json",
+                "http",
+                "query",
+                "javascript",
+                "html",
+                "php",
+                "go",
+                "bash",
+            },
+            auto_install = true,
+            sync_install = false,
+            highlight = { enable = true },
+            indent = { enable = true },
+        })
+    end,
 }
